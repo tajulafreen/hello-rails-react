5c2093f50bb9794071f2759150eb914dc9f4c714
afreen shaik <affushaik9493@gmail.com>
2023-11-01 17:04:49 +0530

    add import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
