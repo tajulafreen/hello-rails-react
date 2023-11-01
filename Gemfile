b293f190d05376cdd2109d603ae3c70bdb45df73
afreen shaik <affushaik9493@gmail.com>
2023-11-01 15:43:07 +0530

    add redux

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+gem 'rubocop', '>= 1.0', '< 2.0'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,20 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+// // Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
